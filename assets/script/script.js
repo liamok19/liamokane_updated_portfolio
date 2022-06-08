71d81e8aa4dfb7269dc067c5355794b4842182c0
Liam O <lokanedeveloper@gmail.com>
2022-06-08 17:33:49 +1000

    pushing correct link

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,29 +3,30 @@ var wdbtn1 = document.getElementById("wd_projectLink0");
 var wdbtn2 = document.getElementById("wd_projectLink1");
 var wdbtn3 = document.getElementById("wd_projectLink2");
 const wdbtn4 = document.getElementById("wd_projectLink3");
+const wdbtn5 = document.getElementById("wd_projectLink4");
 
 wdbtn1.addEventListener("click", (event) => {
-    event.preventDefault();
-    window.open('https://liamok19.github.io/Crypto-Muggles/');
-
-})
+  event.preventDefault();
+  window.open("https://liamok19.github.io/Crypto-Muggles/");
+});
 wdbtn2.addEventListener("click", (event) => {
-    event.preventDefault();
-    window.open('https://liamok19.github.io/weatherDashboard/');
-
-})
+  event.preventDefault();
+  window.open("https://liamok19.github.io/weatherDashboard/");
+});
 wdbtn3.addEventListener("click", (event) => {
-    event.preventDefault();
-    window.open('https://liamok19.github.io/passwordGeneratorJS/');
-
-})
+  event.preventDefault();
+  window.open("https://liamok19.github.io/passwordGeneratorJS/");
+});
 
 wdbtn4.addEventListener("click", (event) => {
-    event.preventDefault();
-    window.open('https://chorestodo.herokuapp.com/');
+  event.preventDefault();
+  window.open("https://chorestodo.herokuapp.com/");
+});
 
-})
-
+wdbtn5.addEventListener("click", (event) => {
+  event.preventDefault();
+  window.open("https://letthemeatcake.herokuapp.com/");
+});
 
 //click function so when user clicks on the image they're directed to the IMDB url of that project
 var animbtn1 = document.getElementById("anim_projectLink0");
@@ -36,33 +37,27 @@ var vfxbtn2 = document.getElementById("vfx_projectLink1");
 var vfxbtn3 = document.getElementById("vfx_projectLink2");
 
 animbtn1.addEventListener("click", (event) => {
-    event.preventDefault();
-    window.open('https://www.imdb.com/title/tt10168312/');
-
-})
+  event.preventDefault();
+  window.open("https://www.imdb.com/title/tt10168312/");
+});
 animbtn2.addEventListener("click", (event) => {
-    event.preventDefault();
-    window.open('https://www.imdb.com/title/tt11792512/?ref_=fn_al_tt_1');
-
-})
+  event.preventDefault();
+  window.open("https://www.imdb.com/title/tt11792512/?ref_=fn_al_tt_1");
+});
 animbtn3.addEventListener("click", (event) => {
-    event.preventDefault();
-    window.open('https://www.imdb.com/title/tt8774798/?ref_=fn_al_tt_1');
-
-})
+  event.preventDefault();
+  window.open("https://www.imdb.com/title/tt8774798/?ref_=fn_al_tt_1");
+});
 
 vfxbtn1.addEventListener("click", (event) => {
-    event.preventDefault();
-    window.open('https://www.imdb.com/title/tt1072748/?ref_=nv_sr_srsg_3');
-
-})
+  event.preventDefault();
+  window.open("https://www.imdb.com/title/tt1072748/?ref_=nv_sr_srsg_3");
+});
 vfxbtn2.addEventListener("click", (event) => {
-    event.preventDefault();
-    window.open('https://www.imdb.com/title/tt3717252/?ref_=nv_sr_srsg_0');
-
-})
+  event.preventDefault();
+  window.open("https://www.imdb.com/title/tt3717252/?ref_=nv_sr_srsg_0");
+});
 vfxbtn3.addEventListener("click", (event) => {
-    event.preventDefault();
-    window.open('https://www.imdb.com/title/tt6499752/?ref_=nv_sr_srsg_0');
-
-})
+  event.preventDefault();
+  window.open("https://www.imdb.com/title/tt6499752/?ref_=nv_sr_srsg_0");
+});
